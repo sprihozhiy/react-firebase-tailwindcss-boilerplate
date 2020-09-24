8c573f6e020bfa26eabb39bae04eb9c1589cffcb
sprihozhiy <sprihozhiy@gmail.com>
2020-09-23 22:22:59 -0600

    added routes and navigation links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+import UserDashboard from "./components/UserDashboard/UserDashboard";
 
 function App() {
   return (
     <div className="h-screen bg-gray-300 flex-col content-between">
       <Header />
       <div className="text-2xl h-screen-main">
-        <SignIn />
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/dashboard" component={UserDashboard} />
+        </Switch>
       </div>
       <Footer />
     </div>
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="mx-auto p-4 h-screen/10 flex justify-between bg-teal-400 font-semibold">
-      <div className="logo text-white">Logo</div>
+    <nav className="mx-auto p-4 h-screen/10 flex justify-between bg-teal-400 font-semibold">
+      <NavLink className="logo text-white" to="/">
+        Logo
+      </NavLink>
       <ul className="flex content-between">
-        <li className="px-1 text-white">Sign Up</li>
-        <li className="px-1 text-white">Sign In</li>
+        <li className="px-1 text-white cursor-pointer hover:underline">
+          <NavLink to="/signup">Sign Up</NavLink>
+        </li>
+        <li className="px-1 text-white cursor-pointer hover:underline">
+          <NavLink to="/">Sign In</NavLink>
+        </li>
+        <li className="px-1 text-white cursor-pointer hover:underline">
+          <NavLink to="/dashboard">Dashboard</NavLink>
+        </li>
       </ul>
-    </div>
+      {/* <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/dashboard" component={UserDashboard} />
+      </Switch> */}
+    </nav>
   );
 };
 
